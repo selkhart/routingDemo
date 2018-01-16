cd53e72f3c657944ffcace44a6c8dd2d901f1204
Sarah Elkhart <selkhart@mail.greenriver.edu>
2018-01-16 16:51:58 -0500

    added views/html files that coorespond with additional paths build

--- index.php
+++ index.php
@@ -1,7 +1,7 @@
 <?php
 
-error_reporting(E_ALL);
-set("display_errors", 1);
+//error_reporting(E_ALL);
+//set("display_errors", 1);
 
 //require the autoload file
 require_once('vendor/autoload.php');
@@ -45,7 +45,31 @@ $f3->route('GET /jewelry/rings/toe-rings', function(){
 //    echo '<h1>Toe Rings</h1>';
 });
 
+//define a route using parameters
+$f3->route('GET /hello/@name',
+    function($f3, $params){
 
+//    $name = $params['name'];
+//    echo"<h1>Hello, $name</h1>";
+
+        //the set function saves information into the 'hive' in fat free
+$f3->set('name',$params['name']);
+$template = new Template();
+echo $template->render('views/hello.html');
+    });
+
+//define a route using parameters
+$f3->route('GET /hi/@first/@last',
+    function($f3, $params){
+
+        //the set function saves information into the 'hive' in fat free
+        $f3->set('first',$params['first']);
+        $f3->set('last',$params['last']);
+        $f3->set('message', 'Hola');
+
+        $template = new Template();
+        echo $template->render('views/hi.html');
+    });
 
 
 //run fat free
