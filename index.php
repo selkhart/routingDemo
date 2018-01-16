1b8b3022eda60fb95c198b23ee61f9311b3305f9
Sarah Elkhart <selkhart@mail.greenriver.edu>
2018-01-16 16:18:14 -0500

    added toe ring route and modified index.php

--- index.php
+++ index.php
@@ -1,5 +1,8 @@
 <?php
 
+error_reporting(E_ALL);
+set("display_errors", 1);
+
 //require the autoload file
 require_once('vendor/autoload.php');
 
@@ -11,12 +14,13 @@ $f3->set('DEBUG',3);
 
 
 
-//define a default route
+//define a default route (home page)
 $f3->route('GET /',function() {
     echo '<h1>Routing Demo</h1>';
 }
 );
 
+//ADDITIONAL ROUTES
 //define a page 1 route
 $f3->route('GET /page1', function(){
     echo '<h1>This is page 1</h1>';
@@ -31,6 +35,15 @@ $f3->route('GET /page1/subpage-a', function(){
 $f3->route('GET /page2', function(){
     echo '<h1>This is page 2</h1>';
 });
+
+////define a route for toe-rings in jewelry/rings subfolders
+//$f3->route('GET /jewelry/rings/toe-rings', function(){
+//    echo '<h1>Toe Rings</h1>';
+//});
+
+//instantiate a template to integrate data into page easier
+$template = new Template();
+echo $template->remder('views/toe-rings.html');
 
 //run fat free
 $f3->run();
